3e8212a06b78e04ddc78f0736d4a299fb387a4e8
Blue Abad <bibo1011@gmail.com>
2020-10-29 18:17:45 -0700

    add user model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const {Model, Datatypes} = require('sequelize');
+const bcrpyt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+            isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [4]
+            }
+        },
+        product_id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {    
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+module.exports = User;
